78c40f4d88a169664c3e6f1c0c838a5f6f257322
George Eaton <george_eaton@hotmail.co.uk>
2017-02-21 16:33:20 +0000

    poster covers video on page load - need to set up button

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ window.onload = function() {
   (function displayYoutubePoster(content) {
     var youtubePoster = document.getElementById("youtube-poster");
     var image = `images/${content.blocks[0].image}`
-    youtubePoster.innerHTML = `<img src=${image}>`
+    youtubePoster.innerHTML = `<img src=${image} >`
   })(pageContent);
 
   (function displayYoutubeVideo(content) {
@@ -11,4 +11,5 @@ window.onload = function() {
     var videoUrl = content.blocks[0].videoId;
     youtubeVideo.innerHTML = "<iframe class='player' frameborder='0' allowfullscreen='1' title='YouTube video player' width='640' height='390' src='https://www.youtube.com/embed/" + videoUrl + "'></iframe>"
   })(pageContent);
+
 }
