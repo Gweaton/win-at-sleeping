d6eedf98f555f17eff816207b7f594b962a2d323
George Eaton <george_eaton@hotmail.co.uk>
2017-02-21 14:52:38 +0000

    displays youtube advert on page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 window.onload = function() {
+
   (function displayYoutubePoster(content) {
     var youtubePoster = document.getElementById("youtube-poster");
     var image = `images/${content.blocks[0].image}`
     youtubePoster.innerHTML = `<img src=${image}>`
+  })(pageContent);
+
+  (function displayYoutubeVideo(content) {
+    var youtubeVideo = document.getElementById("youtube-video");
+    var videoUrl = content.blocks[0].videoId;
+    youtubeVideo.innerHTML = "<iframe class='player' frameborder='0' allowfullscreen='1' title='YouTube video player' width='640' height='390' src='https://www.youtube.com/embed/" + videoUrl + "'></iframe>"
   })(pageContent);
 }
