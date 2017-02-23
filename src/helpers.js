169975b5827ba71bdbe7662a3319bb18a16c1722
George Eaton <george_eaton@hotmail.co.uk>
2017-02-23 15:33:09 +0000

    refactors displayHeadlineContent into two functions

--- src/helpers.js
+++ src/helpers.js
@@ -1,12 +1,14 @@
-function displayHeaderContent(content, header, subheader, blockNumber) {
+function displayHeaderContent(content, header, blockNumber) {
   var header = document.getElementById(header);
   var heading = content.blocks[blockNumber].heading;
+  header.innerHTML = `<h1> ${heading} </h1>`;
+};
+
+function displaySubheaderContent(content, subheader, blockNumber) {
   var subheader = document.getElementById(subheader);
   var subheading = content.blocks[blockNumber].subheading;
-
-  header.innerHTML = `<h1> ${heading} </h1>`;
   subheader.innerHTML = `<p>${subheading}<p>`;
-};
+}
 
 function displayYoutubePoster(content) {
   var youtubePoster = document.getElementById("youtube-poster");
--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 window.onload = function() {
 
   //displays first Header Module
-  displayHeaderContent(pageContent, 'first-header', 'first-subheader', 0);
+  displayHeaderContent(pageContent, 'first-header', 0);
+  displaySubheaderContent(pageContent, 'first-subheader', 0);
+
   //displays youtube Module
   // displayYoutubePoster(pageContent);
   displayYoutubeVideo(pageContent);
 
   //displays second Header Module
-  displayHeaderContent(pageContent, 'second-header', 'second-subheader', 2);
+  displayHeaderContent(pageContent, 'second-header', 2);
+  displaySubheaderContent(pageContent, 'second-subheader', 2);
 
   //displays Idea Grid Module
   displayIdeaGridContent(pageContent, "ideas", 3);
   displayLink(pageContent, 3);
 
   //displays Product Grid Module
+  displayHeaderContent(pageContent, 'third-header', 4);
   displayProductGridContent(pageContent, "products", 4);
 
   //displays Carousel Module
-  displayHeaderContent(pageContent, 'third-header', 'third-subheader', 5);
+  displayHeaderContent(pageContent, 'fourth-header', 5);
+  displaySubheaderContent(pageContent, 'fourth-subheader', 5)
   displayCarouselContent(pageContent, 6);
   showSlides(1);
 }
