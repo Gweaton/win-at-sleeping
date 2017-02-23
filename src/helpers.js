bb692b5767f632dcb88c2779e4432b5da52f49ec
George Eaton <george_eaton@hotmail.co.uk>
2017-02-23 19:18:06 +0000

    adds quotes to coach tips

--- src/helpers.js
+++ src/helpers.js
@@ -58,6 +58,6 @@ function displayCarouselContent(content, blockNumber) {
     var heading = item.heading;
     var text = item.text;
     var images = item.images;
-    carousel.innerHTML += "<div class='carousel-item fade'><div class='item-details'><h2>" + heading + "</h2> <h3>IKEA Sleep Coach says: </h3><p>" + text +"</p></div><div class='seq-image'><img class='first' src='images/" + images[0] +"'><img class='second' src='images/" + images[1] +"'></div></div>"
+    carousel.innerHTML += "<div class='carousel-item fade'><div class='item-details'><h2>" + heading + "</h2> <h3>IKEA Sleep Coach says: </h3><p>\"" + text +"\"</p></div><div class='seq-image'><img class='first' src='images/" + images[0] +"'><img class='second' src='images/" + images[1] +"'></div></div>"
   });
 };
