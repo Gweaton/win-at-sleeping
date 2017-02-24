8048712038a2ba454a82ccb637acae80bc6936f0
George Eaton <george_eaton@hotmail.co.uk>
2017-02-24 10:03:46 +0000

    adds missing link to page and json file

--- src/index.js
+++ src/index.js
@@ -14,15 +14,16 @@ window.onload = function() {
 
   //displays Idea Grid Module
   displayIdeaGridContent(pageContent, "ideas", 3);
-  displayLink(pageContent, 3);
+  displayLink(pageContent, "sleep-ideas", 3);
 
   //displays Product Grid Module
   displayHeaderContent(pageContent, 'third-header', 4);
   displayProductGridContent(pageContent, "products", 4);
+  displayLink(pageContent, 'product-ideas', 5)
 
   //displays Carousel Module
-  displayHeaderContent(pageContent, 'fourth-header', 5);
-  displaySubheaderContent(pageContent, 'fourth-subheader', 5)
-  displayCarouselContent(pageContent, 6);
+  displayHeaderContent(pageContent, 'fourth-header', 6);
+  displaySubheaderContent(pageContent, 'fourth-subheader', 6)
+  displayCarouselContent(pageContent, 7);
   showSlides(1);
 }
